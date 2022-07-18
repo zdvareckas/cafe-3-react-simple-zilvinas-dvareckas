7007422830859d28d1a9b982e84716c454b067ed
zilvinas-dvareckas <95238038+zdvareckas@users.noreply.github.com>
2022-07-18 20:54:26 +0300

    (feat): added Eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "airbnb",
+        "airbnb/hooks"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "react/prop-types": [0],
+        "newline-before-return": [1],
+        "import/prefer-default-export": [0],
+        "react/function-component-definition": [1, {
+            "namedComponents": "arrow-function"
+        }],
+    }
+}
