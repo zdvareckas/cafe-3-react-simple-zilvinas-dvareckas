f110dc166d218cf488d26b1c24184ae63dd667b3
zilvinas-dvareckas <95238038+zdvareckas@users.noreply.github.com>
2022-07-26 17:09:33 +0300

    feat: rough data dispaly in rental-bikes page

--- src/pages/rental-bikes-page/index.jsx
+++ src/pages/rental-bikes-page/index.jsx
@@ -1,4 +1,13 @@
 import React from 'react';
+import {
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from '@mui/material';
 import RentalBikeContext from '../../contexts/rental-bikes-context';
 
 const RentalBikesPage = () => {
@@ -10,11 +19,41 @@ const RentalBikesPage = () => {
       .then((fetchedBikes) => setBikes(fetchedBikes));
   }, []);
 
-  console.log(bikes);
   console.log('RentalBikesPage, rentalBikeContextValue', rentalBikeContext);
 
   return (
-    <div>Rental-Bikes page</div>
+    <Box sx={{
+      display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: 2, mt: 10, placeItems: 'center',
+    }}
+    >
+      {bikes.map(({
+        id, title, description, category, img,
+      }) => (
+        <Card key={id} sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            height="200"
+            image={img}
+            alt={title}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {description}
+            </Typography>
+            <Typography color="text.secondary">
+              {category}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Peržiūrėti</Button>
+            <Button size="small">Pridėti į užsakymą</Button>
+          </CardActions>
+        </Card>
+      ))}
+    </Box>
   );
 };
 
