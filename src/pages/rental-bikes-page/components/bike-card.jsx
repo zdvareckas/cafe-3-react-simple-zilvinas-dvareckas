6ebaa31af8046427be19b442a352935127f3f110
zilvinas-dvareckas <95238038+zdvareckas@users.noreply.github.com>
2022-07-31 14:28:11 +0300

    feat: added useState hook to bike size select

--- src/pages/rental-bikes-page/components/bike-card.jsx
+++ src/pages/rental-bikes-page/components/bike-card.jsx
@@ -20,46 +20,52 @@ const BikeCard = ({
   description,
   category,
   sizes,
-}) => (
-  <Card key={id}>
-    <Box>
-      <Image component="img" src={img} />
-    </Box>
+}) => {
+  const [bikeSize, setBikeSize] = React.useState('');
 
-    <CardContent sx={{
-      display: 'flex', flexDirection: 'column', gap: 2,
-    }}
-    >
-      <Typography gutterBottom variant="h6" component="div">
-        {title}
-      </Typography>
-      <Typography variant="body1">
-        {description}
-      </Typography>
-      <Chip size="small" color="primary" label={category} sx={{ width: '25%' }} />
-      <FormControl size="small">
-        <InputLabel id="size-select">Dydis</InputLabel>
-        <Select
-          labelId="size-select"
-          id="size-select"
-          label="Dydis"
-        >
-          {sizes.map((size) => (
-            <MenuItem key={size}>
-              {size}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </CardContent>
-    <Box sx={{
-      display: 'flex', py: 1, justifyContent: 'center', gap: 2,
-    }}
-    >
-      <Button variant="contained">Peržiūrėti</Button>
-      <Button variant="contained">Užsakyti</Button>
-    </Box>
-  </Card>
-);
+  return (
+    <Card key={id}>
+      <Box>
+        <Image component="img" src={img} />
+      </Box>
+
+      <CardContent sx={{
+        display: 'flex', flexDirection: 'column', gap: 2,
+      }}
+      >
+        <Typography gutterBottom variant="h6" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body1">
+          {description}
+        </Typography>
+        <Chip size="small" color="primary" label={category} sx={{ width: '25%' }} />
+        <FormControl size="small">
+          <InputLabel id="size-select">Dydis</InputLabel>
+          <Select
+            labelId="size-select"
+            id="size-select"
+            label="Dydis"
+            value={bikeSize}
+            onChange={(e) => setBikeSize(e.target.value)}
+          >
+            {sizes.map((size) => (
+              <MenuItem key={size} value={size}>
+                {size}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </CardContent>
+      <Box sx={{
+        display: 'flex', py: 1, justifyContent: 'center', gap: 2,
+      }}
+      >
+        <Button variant="contained">Peržiūrėti</Button>
+        <Button variant="contained">Užsakyti</Button>
+      </Box>
+    </Card>
+  );
+};
 
 export default BikeCard;
