f98f5035f27a4b14d26f0b4a13a3513937a108c7
zilvinas-dvareckas <95238038+zdvareckas@users.noreply.github.com>
2022-08-02 15:45:19 +0300

    refactor: removed duplication in contact-info-section  component

--- src/pages/contacts-page/components/contact-info-section.jsx
+++ src/pages/contacts-page/components/contact-info-section.jsx
@@ -1,29 +1,49 @@
 import * as React from 'react';
 import {
-  Box, Typography,
+  Box,
+  Typography,
 } from '@mui/material';
 import CallIcon from '@mui/icons-material/Call';
 import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import { ContactField } from '.';
 
+const contacts = [
+  {
+    id: '1',
+    text: '+37061111111',
+    icon: <CallIcon />,
+  },
+  {
+    id: '2',
+    text: 'antanodviračiai@dviračiai.lt',
+    icon: <AlternateEmailIcon />,
+  },
+  {
+    id: '3',
+    text: 'M. K. Čiurlionio g. 100, Vilnius 03100',
+    icon: <LocationOnIcon />,
+  },
+
+];
+
 const ContactInfoSection = () => (
-  <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
+  <Box sx={{
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 1,
+  }}
+  >
     <Typography variant="body2" align="left">
       Kontaktinė informacija
     </Typography>
-    <ContactField>
-      <CallIcon />
-      +37061111111
-    </ContactField>
-    <ContactField>
-      <AlternateEmailIcon />
-      antanodviračiai@dviračiai.lt
-    </ContactField>
-    <ContactField>
-      <LocationOnIcon />
-      M. K. Čiurlionio g. 100, Vilnius 03100
-    </ContactField>
+    {contacts.map(({ id, text, icon }) => (
+      <ContactField key={id}>
+        {icon}
+        {text}
+      </ContactField>
+    ))}
+
   </Box>
 );
 
