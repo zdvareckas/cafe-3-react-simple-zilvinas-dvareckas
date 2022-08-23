a0a1b7f2b3a0c61f6b6c736d5b06d700ab943b6b
zilvinas-dvareckas <95238038+zdvareckas@users.noreply.github.com>
2022-08-23 21:41:44 +0300

    fix: fixed bikes display in rental-bikes-page

--- src/pages/rental-bikes-page/index.jsx
+++ src/pages/rental-bikes-page/index.jsx
@@ -6,10 +6,12 @@ import { ItemsGrid } from './components';
 const RentalBikesPage = () => {
   const [bikes, setBikes] = React.useState([]);
   React.useEffect(() => {
-    fetch('http://localhost:8000/bikes')
+    fetch('http://localhost:8000/bikes?_expand=category&_expand=driver')
       .then((res) => res.json())
       .then((fetchedBikes) => setBikes(fetchedBikes));
   }, []);
+
+  console.log(bikes);
 
   return (
     <>
@@ -30,10 +32,10 @@ const RentalBikesPage = () => {
             id={id}
             title={title}
             description={description}
-            category={category}
+            category={category.label}
             img={img}
             sizes={sizes}
-            driver={driver}
+            driver={driver.label}
             price={price}
           />
         ))}
