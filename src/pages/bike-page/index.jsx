49c93dda19e47118029e6ee127dae25e2d0b5feb
zilvinas-dvareckas <95238038+zdvareckas@users.noreply.github.com>
2022-09-05 14:35:55 +0300

    feat: displaying  bike object in a page

--- src/pages/bike-page/index.jsx
+++ src/pages/bike-page/index.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { bikeService } from '../../services/bike-service';
 
-const BikePage = () => (
-  <div>BikePage</div>
-);
+const BikePage = () => {
+  const { bikeId } = useParams();
+  const [bike, setBike] = React.useState(null);
+
+  React.useEffect(() => {
+    (async () => {
+      const response = await bikeService.fetchById(bikeId);
+      setBike(response);
+    })();
+  }, [bikeId]);
+
+  console.log(bikeId);
+
+  return (
+    <pre>{JSON.stringify(bike, null, 4)}</pre>
+  );
+};
 
 export default BikePage;
